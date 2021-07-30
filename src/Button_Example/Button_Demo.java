8b0b71962da0ed08cac48503b88b13222503988d
Sunil S <sunil2332000@gmail.com>
2021-07-30 16:53:22 +0530

    Button_Demo.java is uploaded

---
+++ src/Button_Example/Button_Demo.java
@@ -0,0 +1,49 @@
+package Button_Example;
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Button_Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "D:\\selenium tutorial\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://www.leafground.com/pages/Button.html");
+		
+		//1.Go to home page
+				WebElement homeButton=driver.findElement(By.id("home"));
+				homeButton.click();
+		
+		//2.Get the xy position
+		WebElement getPositionButton=driver.findElement(By.id("position"));
+		Point xypoint=getPositionButton.getLocation();
+		int xValue=xypoint.getX();
+		int yValue=xypoint.getY();
+		System.out.println("X value is:"+xValue+"Y value is:"+yValue);
+		
+		//3.Find Button color
+		WebElement colorButton=driver.findElement(By.id("color"));
+		String color=colorButton.getCssValue("background-color");
+		System.out.println("Button color is:"+color);
+		
+		//4.Find size
+		WebElement sizeButton=driver.findElement(By.id("size"));
+		int height=sizeButton.getSize().getHeight();
+		int width=sizeButton.getSize().getWidth();
+		System.out.println("Height is:"+height+"width is:"+width);
+		
+		
+		
+		
+		
+	
+	
+
+	}
+
+}
