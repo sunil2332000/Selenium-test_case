5e0ab643eb223b700df68a19924d284959c73ec2
Sunil S <sunil2332000@gmail.com>
2021-07-30 11:39:06 +0530

    Changes corrected

---
+++ src/google_title_check/Opengoogle.java
@@ -0,0 +1,25 @@
+package selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Opengoogle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1.open chrome
+		//2.google home page
+		System.setProperty("webdriver.chrome.driver", "D:\\selenium tutorial\\chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://www.google.co.in");
+		String google;
+		google=driver.getTitle();
+		System.out.println(google);
+		//
+
+	}
+
+}
+
