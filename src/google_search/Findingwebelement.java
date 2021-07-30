7c3279d4750f89762bdf9c820777972ce5552bce
Sunil S <sunil2332000@gmail.com>
2021-07-30 11:50:48 +0530

    Fixed Merge Conflict

--- src/google_search/Findingwebelement.java
+++ src/google_search/Findingwebelement.java
@@ -1,8 +1,6 @@
-<<<<<<< HEAD:src/google_search/Findingwebelement.java
+
 package google_search;
-=======
-package webelement;
->>>>>>> 5e0ab643eb223b700df68a19924d284959c73ec2:src/webelement/Findingwebelement.java
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -16,7 +14,7 @@ public class Findingwebelement {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://www.google.co.in");
 		//enter a search term
-		//click the wikipedia link
+		
 		driver.findElement(By.name("q")).sendKeys("prithvi missile" + Keys.ENTER);
 		driver.get("https://en.wikipedia.org ");
 		driver.findElement(By.linkText("Prithvi (missile) - Wikipedia")).click();
